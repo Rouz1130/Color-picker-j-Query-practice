52eebab73f6c7ffb607c298dffcde34642dab63f
rouz majlessi <rmajlessi@gmail.com>
2017-03-16 22:59:59 -0700

    refactor $ replace JQuery shortcut

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 // this new line is so that our script tags in our html can run until the document is finished loading.
-jQuery(document).ready(function(){
-  jQuery("H1").click(function(){
+$(document).ready(function(){
+  $("H1").click(function(){
     // alert is a callback function.
     alert("This is a header.");
   });
-  // we pass jQuery("h1") function to the .click mehtod fucntion. this is called a callback when we pass a function as an argument to another function.
+  // we pass $("h1") function to the .click mehtod fucntion. this is called a callback when we pass a function as an argument to another function.
 
 
 
   // JQuery  selects HTML elembernts on the page base on the argument passed in. example below is a p therefore all p tags are clickable.
 
   // the click method attaced is an event handler.
-  jQuery("p").click(function(){
+  $("p").click(function(){
 
     alert("This is a paragraph.");
   });
 
-  jQuery("img").click(function(){
+  $("img").click(function(){
     alert("This is an image.");
   });
 });
