232fb56b90d8d3a9bca793470a1eff5ad12d2d0b
rouz majlessi <rmajlessi@gmail.com>
2017-03-16 22:46:13 -0700

    add jquery to show alert on header, p andimage tags

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// alert is always two spaces indented inside
+// anythig inside a function is indented two spaces.
+jQuery("H1").click(function(){
+  alert("This is a header.");
+});
+
+jQuery("p").click(function(){
+  alert("This is a paragraph.");
+});
+
+jQuery("img").click(function(){
+  alert("This is an image.");
+});
