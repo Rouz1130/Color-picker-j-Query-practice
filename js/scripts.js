2d70cfc0c84b069650d898d83695200754dda3ea
rouz majlessi <rmajlessi@gmail.com>
2017-03-16 22:58:45 -0700

    take out js and jquery tags put them in Head section and because we did so we have to now write the document ready to start our jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-// alert is always two spaces indented inside
-// anythig inside a function is indented two spaces.
-jQuery("H1").click(function(){
-  alert("This is a header.");
-});
+// this new line is so that our script tags in our html can run until the document is finished loading.
+jQuery(document).ready(function(){
+  jQuery("H1").click(function(){
+    // alert is a callback function.
+    alert("This is a header.");
+  });
+  // we pass jQuery("h1") function to the .click mehtod fucntion. this is called a callback when we pass a function as an argument to another function.
 
-jQuery("p").click(function(){
-  alert("This is a paragraph.");
-});
 
-jQuery("img").click(function(){
-  alert("This is an image.");
+
+  // JQuery  selects HTML elembernts on the page base on the argument passed in. example below is a p therefore all p tags are clickable.
+
+  // the click method attaced is an event handler.
+  jQuery("p").click(function(){
+
+    alert("This is a paragraph.");
+  });
+
+  jQuery("img").click(function(){
+    alert("This is an image.");
+  });
 });
